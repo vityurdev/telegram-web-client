e249a396b7cbede9032856c3d9daefb37ca73c02
Vitaly Yurenya <vitaly.yurenya@gmail.com>
2018-09-12 00:24:06 +0300

    Spinner: WIP snapshot

---
+++ src/components/Spinner/Spinner.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export const smth = null;
+
+class Spinner extends Component {
+  render() {
+    return (
+      <></>
+    )
+  }
+}
+
+export default Spinner;
